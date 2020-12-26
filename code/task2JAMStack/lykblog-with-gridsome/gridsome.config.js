3b50de306a35c45e77ffaf68aa1997434d0b77fc
lyka3156 <1210167160@qq.com>
2020-12-26 22:48:54 +0800

    feat: 我的博客

--- code/task2JAMStack/lykblog-with-gridsome/gridsome.config.js
+++ code/task2JAMStack/lykblog-with-gridsome/gridsome.config.js
@@ -6,11 +6,48 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: [{
-    use: '@gridsome/source-filesystem',
-    options: {
-      typeName: 'BlogPost',     // 数据类型
-      path: './content/blog/**/*.md', // 从哪里抓取mk文件
+  plugins: [
+    // 读取本地文件转换成markdown的配置
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'BlogPost',     // 数据类型
+        path: './content/blog/**/*.md', // 从哪里抓取mk文件
+      }
+    },
+    // 将strapi中的数据配置到gridsome中
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        queryLimit: 1000, // (最多查询条数)
+        // 查询strapi集合的
+        contentTypes: ['post', 'tag'], // 查询的实体
+        // typeName:"Strapi",   // 类型名称默认Strapi
+        // 查询strapi单个节点的
+        singleTypes: ['general'], // 单个节点类型
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // 莫些数据受保护的，需要登录
+        // loginData: {
+        //   identifier: '',
+        //   password: ''
+        // }
+      }
     }
-  }],
+  ],
+  // 配置模板组件 (这里配置的组件会被打包生成静态页面)
+  templates: {
+    // 内容类型 = plugins中的typeName+contentTypes
+    StrapiPost: [{
+      path: "/post/:id", // 文章详情路劲
+      component: "./src/templates/Post.vue" // 通过这个模板生成静态页面了
+    }],
+    StrapiTag: [
+      {
+        path: "/tag/:id", // 文章详情路劲
+        component: "./src/templates/Tag.vue" // 通过这个模板生成静态页面了
+      }
+    ]
+  }
 }
